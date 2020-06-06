7061d5101f47bccd93971bd60f061c242b447d45
Luke Johnson <lukejohnson3712@gmail.com>
2020-06-06 18:03:34 -0500

    Reorder.

--- database/to_do_list.sql
+++ database/to_do_list.sql
@@ -1,8 +1,8 @@
 CREATE TABLE "list"
 (
     "task_id" serial primary key,
-    "title" varchar(200) NOT NULL,
     "completed" BOOLEAN DEFAULT FALSE,
+    "title" varchar(200) NOT NULL,
     "priority" varchar(3) DEFAULT NULL,
     CONSTRAINT priority_exclamation_mark_only CHECK (priority IN ('!', '!!', '!!!')),
     "due" DATE DEFAULT NULL,
