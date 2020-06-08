bd7b2c32d5d0cb5f97295f1b7c8adb51d2bd79bb
Luke Johnson <lukejohnson3712@gmail.com>
2020-06-08 12:57:07 -0500

    Add example tasks.

--- database/to_do_list.sql
+++ database/to_do_list.sql
@@ -8,3 +8,11 @@ CREATE TABLE "list"
     "due" TIMESTAMP DEFAULT NULL,
     "notes" varchar(500)
 );
+
+INSERT INTO "list" ("title", "priority", "due", "notes") VALUES ('Title 1', '!', '06/10/2020 16:50', 'Notes about the task.');
+INSERT INTO "list" ("title", "priority", "due") VALUES ('Title 2', '!!', '06/10/2020 17:50');
+INSERT INTO "list" ("title", "priority", "notes") VALUES ('Title 3', '!!!', 'Different notes.');
+INSERT INTO "list" ("title", "due") VALUES ('Title 4', '06/10/2020 14:50');
+INSERT INTO "list" ("title") VALUES ('Title 5');
+INSERT INTO "list" ("title") VALUES ('Title 6');
+INSERT INTO "list" ("title") VALUES ('Title 7');
